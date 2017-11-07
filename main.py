b2f598527a64a4b483a00d8c7a13bcf63cd3ef7c
Vitor Seger Zeni <vsz@certi.org>
2017-11-07 15:12:25 -0200

    First program structure

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  sem título.py
-#  
-#  Copyright 2017 vsz <vsz@CES-11707>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+import sys
 
+def getRandomRecipientsFromFile() :
+	recipients = {};
+	recipients.update({"Mr. Vits" : "vszeni@gmail.com"})
+	recipients.update({"Mrs. Pipi" : "fernanda@almeidamarcon.com"})
+	
+	return recipients
+	
+def sendEmailToRecipient(recipients) :
+	for r in recipients.keys() :
+		print('E-mail was sent to {} at {}'.format(r, recipients[r]))
+
+def sendRandomEmail() :
+	recipients = getRandomRecipientsFromFile()
+	sendEmailToRecipient(recipients)
+	
 
 def main(args):
     return 0
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+
+
+if __name__ == '__main__' :
+	print("Starting...")
+	sendRandomEmail()
+	input("Press Enter to continue")
+	sys.exit(main(sys.argv))
